8062f42dd2de270b6129e30371402b8466be7f48
Alex Martinez <alexmartinez1745@gmail.com>
2021-01-10 21:38:20 -0500

    start of money code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,12 @@
 #import operating system and csv
 import os
 import csv
+#crate variables that hold lists
+month = []
+TotalMoney = []
+ChangeAverage = []
+HighestProfit = []
+LowestProfit = []
 #create path for csv file
 csvpath = os.path.join("Resources", "budget_data.csv")
 #reading through the csv path with reader..
@@ -8,8 +14,9 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     budgetheader = next(csvreader)
     print("-----------------------------------------------")
-    month = []
     #looping through the rows to find total number of months
     for row in csvreader:
         month.append(row[0])
-        print((len(month)))
+        print(len(month))
+        TotalMoney.append(row[1])
+        print(sum(TotalMoney))
