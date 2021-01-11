2d9c91e5706fd1e767e959ac94e3f692a08f4da2
Alex Martinez <alexmartinez1745@gmail.com>
2021-01-11 16:36:13 -0500

    adding chane of profit code

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,7 @@ import csv
 #crate variables that hold lists
 month = []
 TotalMoney = []
-ChangeAverage = []
+Change = [] 
 HighestProfit = []
 LowestProfit = []
 #create path for csv file
@@ -16,7 +16,13 @@ with open(csvpath) as csvfile:
     print("-----------------------------------------------")
     #looping through the rows to find total number of months
     for row in csvreader:
+        #add to each month row to find total months
         month.append(row[0])
-        print(len(month))
-        TotalMoney.append(row[1])
-        print(sum(TotalMoney))
+        #calculate total by summing up row 1
+        TotalMoney.append(int(row[1]))
+        #calculate changes per row of profit/loss and find the average
+    for i in range(len(TotalMoney)-1):
+        Change.append(TotalMoney[i+1] - TotalMoney[i])
+print(f'Total Months: {len(month)}')        
+print(f'Total: {sum(TotalMoney)}')
+print(f'Change: {sum(Change)}')
