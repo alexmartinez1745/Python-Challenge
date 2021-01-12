4b957cef5d68ab15477d0693284619b8f72d0a1f
Alex Martinez <alexmartinez1745@gmail.com>
2021-01-12 12:32:32 -0500

    max vote code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,8 @@ Canidates = []
 CanidateVotes = []
 percentagevotes = []
 percent = 0
+Winner = []
+maxvotes = 0
 #create path for csv file
 csvpath = os.path.join("Resources", "election_data.csv")
 #read through the csv path with reader
@@ -16,7 +18,7 @@ with open(csvpath) as csvfile:
     #loop through rows to find total number of votes
     for row in csvreader:
         TotalVotes.append(row[0])
-        #set variable to find different canidate names in row 2
+        #set variable to find different canidate names in row 2 of csv
         canidatename = row[2]
         #create conditional to find unique canidate name and add each vote
         #if not in current canidate move to the next canidate
@@ -30,7 +32,8 @@ with open(csvpath) as csvfile:
     for votecount in range(len(Canidates)):
         percent = round(((CanidateVotes[votecount]) / (len(TotalVotes))) * 100 , 2)
         percentagevotes.append(percent)
-        
+        if maxvotes > max(votecount):
+            Winner.append(maxvotes)
         
 #print putcomes to summary table
 print("Election Results")
@@ -39,3 +42,4 @@ print(f"Total Votes: {len(TotalVotes)}")
 print("------------------------------")
 print(f'{list(Canidates)} {(CanidateVotes)}: ')
 print(f"Percentage: {percentagevotes}%")
+print(f"Winner: {Winner}")
