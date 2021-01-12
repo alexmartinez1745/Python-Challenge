63abf7660bd7d9df82a4d76cc4a6814b3962423f
Alex Martinez <alexmartinez1745@gmail.com>
2021-01-12 00:51:26 -0500

    calculating percentage code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,8 @@ import csv
 TotalVotes = []
 Canidates = []
 CanidateVotes = []
+percentagevotes = []
+percent = 0
 #create path for csv file
 csvpath = os.path.join("Resources", "election_data.csv")
 #read through the csv path with reader
@@ -14,13 +16,22 @@ with open(csvpath) as csvfile:
     #loop through rows to find total number of votes
     for row in csvreader:
         TotalVotes.append(row[0])
+        #set variable to find different canidate names in row 2
         canidatename = row[2]
+        #create conditional to find unique canidate name and add each vote
+        #if not in current canidate move to the next canidate
         if canidatename in Canidates:
             canidateindex = Canidates.index(canidatename)
             CanidateVotes[canidateindex] = CanidateVotes[canidateindex] + 1
         else:
             Canidates.append(canidatename)
             CanidateVotes.append(1)
+    for votecount in range(len(Canidates)):
+        percent = ((CanidateVotes[votecount]) / (len(TotalVotes))) * 100
+        percentagevotes.append(percent)
+        
+        
 #print putcomes to summary table
 print(f"Total Votes: {len(TotalVotes)}")
-print(f'{list(Canidates)} {CanidateVotes}')
+print(f'{list(Canidates)} {(CanidateVotes)}: ')
+print(f"Percentage: {percentagevotes}")
